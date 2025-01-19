96ef082c434e6b4c0acc3a922e0107452f02ffce
Alex Belanger <alex.belanger55@gmail.com>
2025-01-18 19:30:34 -0500

    Revert "changed the app"
    This reverts commit 4ed3f4f7e79204348b206913b470ebc2d71e8879.

--- classmind/api/models/session/schema.gadget.ts
+++ classmind/api/models/session/schema.gadget.ts
@@ -1,16 +1,16 @@
 import type { GadgetModel } from "gadget-server";
 
-// This file describes the schema for the "session" model, go to https://school.gadget.app/edit to view/edit your model in Gadget
+// This file describes the schema for the "session" model, go to https://classmind.gadget.app/edit to view/edit your model in Gadget
 // For more information on how to update this file http://docs.gadget.dev
 
 export const schema: GadgetModel = {
   type: "gadget/model-schema/v1",
-  storageKey: "FSHOdzLPjHkr",
+  storageKey: "_SJEHP8WNSj2",
   fields: {
     user: {
       type: "belongsTo",
       parent: { model: "user" },
-      storageKey: "4fx5gKfdrP8e",
+      storageKey: "XL7P3noDM801",
     },
   },
 };
--- classmind/web/api.js
+++ classmind/web/api.js
@@ -1,5 +1,5 @@
 // Sets up the API client for interacting with your backend. 
-// For your API reference, visit: https://docs.gadget.dev/api/school
-import { Client } from "@gadget-client/school";
+// For your API reference, visit: https://docs.gadget.dev/api/classmind
+import { Client } from "@gadget-client/classmind";
 
 export const api = new Client({ environment: window.gadgetConfig.environment });
